32d0c4f59dee0c5ff6296333dd83a766cf157df2
Kaio Moura <kaioabrahaonepo@gmail.com>
2022-12-07 17:26:23 -0300

    Inserção da logo e organização dos arquivos

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,9 +1,8 @@
 const App = () => {
   return (
     <div className="App">
-      <img src="https://github.com/"/>
+      <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="logo" height={72} width={72}/>
     </div>
   );
 }
-
 export default App;
